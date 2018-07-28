8cb4c0a2dc2a71b551a7e57c81a481040bc7d722
Pauline CAPESTAN <pcapesta@e3r8p3.42.fr>
2018-07-28 20:22:01 +0200

    Ca marrrhce

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: pcapesta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 21:53:52 by pcapesta          #+#    #+#             */
-/*   Updated: 2018/07/22 00:00:50 by pcapesta         ###   ########.fr       */
+/*   Updated: 2018/07/28 20:20:18 by pcapesta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	static char		*rest = NULL;
 	char			buf[BUFF_SIZE + 1];
@@ -29,13 +29,14 @@ int get_next_line(const int fd, char **line)
 
 	if (rest)
 	{
-		//printf("coucou!\n");
+		//printf("Il y a un reste\n");
 		if (!(ft_strchr(rest, '\n')))
 		{
 			ret = read(fd, buf, BUFF_SIZE);
 			if (ret == 0)
 			{
 				*line = ft_strdup(rest);
+				ft_memdel((void**)&rest);
 				return (1);
 			}
 			else if (ret == -1)
@@ -52,7 +53,6 @@ int get_next_line(const int fd, char **line)
 	}
 	else if (!(rest))
 	{
-		//printf("coucou\n");
 		ret = read(fd, buf, BUFF_SIZE);
 		if (ret == 0)
 			return (0);
@@ -71,38 +71,46 @@ int get_next_line(const int fd, char **line)
 		if (ret == 0)
 		{
 			*line = ft_strdup(interm);
+			free(interm);
+			free(gnl);
 			return (1);
 		}
 		else if (ret == -1)
+		{
+			free(interm);
+			free(gnl);
 			return (-1);
+		}
 		else if (ret != -1)
 		{
 			buf[ret] = '\0';
 			gnl = ft_strjoin(interm, buf);
 		}
 	}
+	free(interm);
 	while (gnl[i] && gnl[i] != '\n')
 		i++;
 	*line = strndup(gnl, i);
-	rest = ft_strsub(gnl, i + 1, ft_strlen(gnl) - (i + 1));
-	rest[0] = 0;
+	if (gnl[i] != '\0' && gnl[i + 1] != '\0')
+		rest = ft_strsub(gnl, i + 1, ft_strlen(gnl) - (i + 1));
+	else
+		ft_memdel((void**)&rest);
+	free(gnl);
 	return (1);
 }
-\0 = 0 = NULL
-'0' = 48
 
-int		main()
+/*int		main()
 {
 	int fd;
 	char *ou;
 	int		ret = 0;
 	int i = 0;
 
-	fd = open("text.c", O_RDONLY);
-	while (i < 3)
+	fd = open("test1", O_RDONLY);
+	ret = get_next_line(fd, &ou);
+	while (ret > -1)
 	{
 		ret = get_next_line(fd, &ou);
-		i++;
 	}
 }
-
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcapesta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/28 20:20:56 by pcapesta          #+#    #+#             */
+/*   Updated: 2018/07/28 20:20:59 by pcapesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,7 +20,7 @@
 # include <unistd.h>
 # include "./libft/libft.h"
 
-# define BUFF_SIZE 28
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
